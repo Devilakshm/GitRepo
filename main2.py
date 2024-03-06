09ef1a8589225500ae3a919c85839ec052c0f1a4
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-06 16:44:09 +0530

    maximumvalues sum returns

--- main2.py
+++ main2.py
@@ -1,4 +1,8 @@
-ls=[54,67,89]
-insert=98
-ls.insert(2,insert)
-print(ls)
+def max_l(a):
+    max_values=[]
+    for i in a:
+        max_value=max(i)
+        max_values.append(max_value)
+    return sum(max_values)
+a=[[23,4,5,6],[54,6,7],[9,6,3]]
+print(max_l(a))
