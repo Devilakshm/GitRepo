d39f49bdc20d2de851c30d9a2d34e159a446591c
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-07 15:54:17 +0530

    filter method used in lambda function

---
+++ lambda.py
@@ -0,0 +1,8 @@
+lst=[23,4,65,67,96]
+la=list(filter(lambda x: x%2==0,lst))
+print(la)
+
+#odd elements
+lst=[23,4,65,67,96]
+la=list(filter(lambda x: x%2!=0,lst))
+print(la)
--- while.py
+++ while.py
@@ -8,3 +8,9 @@ while i<5:
 lst = [12, 4, 45, 67, 90]
 for i in lst:
     print(i)
+#range
+print(list(range(2,10)))
+#adding one element
+lst=[23,5,6,8,35]
+lst=[x+1 for x in lst]
+print(lst)
